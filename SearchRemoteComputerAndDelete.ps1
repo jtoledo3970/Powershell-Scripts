63ae998c5634c455920701e3db6ed952e8404cdd
Jose Toledo <jtoledo3970@users.noreply.github.com>
2021-06-21 22:47:36 -0700

    initial

---
+++ SearchRemoteComputerAndDelete.ps1
@@ -0,0 +1,10 @@
+<#
+Created by: Jose M Toledo
+Purpose: Search a list of remote computers for a specific file and delete it
+#>
+
+$computers = Get-Content "c:\temp\computernames.txt"
+
+foreach ($computer in $computers) {
+
+}
